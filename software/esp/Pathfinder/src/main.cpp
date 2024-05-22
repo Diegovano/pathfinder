d37cca34b77c3b472b44367a187afba47c2847a1
Andres Eduardo Martinez Llorente <andres.m.ll2003@outlook.com>
2024-05-22 14:41:50 +0100

    fix: Adjust Slave::spi_tx_string() to 4 byte buffers

--- software/esp/Pathfinder/src/main.cpp
+++ software/esp/Pathfinder/src/main.cpp
@@ -3,9 +3,10 @@
 #include <string>
 #include <WiFi.h>
 #include <esp_wpa2.h>
-#include <Graph.hpp>
+#include <Graph.h>
+#include <Slave.h>
 
-const int SPI_SS = D2; // Slave select is active-low
+const int SPI_CS = D2; // Slave select is active-low
 
 // WPA2 Enterprise Authentication
 /*
@@ -40,18 +41,17 @@ const char *ROOT_CA = "-----BEGIN CERTIFICATE-----\n" \
 
 // WPA2 Personal Authentication
 
-const char *PER_SSID = "AEML_iphone";
-const char *PER_PASSWORD = "password";
+const char *PER_SSID = "LAPTOP-GLC7SFN7-8537";
+const char *PER_PASSWORD = "U00#10s0";
 
 // Server declaration
 WiFiServer server(80);
 WiFiClient client;
-String request;
+std::string request;
 
 // LED states
 const int LED = D0;
 int LEDState;
-
 
 // Current time
 unsigned long currentTime = millis();
@@ -60,27 +60,7 @@ unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
-const int NUM_CHARS = 3;
-
-char TX_BUF[NUM_CHARS];
-uint8_t RX_BUF[NUM_CHARS];
-
-void control(String request)
-{
-  // turns the GPIOs on and off
-  if (request.indexOf("GET /LED26_ON") >= 0) 
-  {
-    Serial.println("GPIO 26 on");
-    LEDState = HIGH;
-    digitalWrite(LED, HIGH);
-  } 
-  else if (request.indexOf("GET /LED26_OFF") >= 0) 
-  {
-    Serial.println("GPIO 26 off");
-    LEDState = LOW;
-    digitalWrite(LED, LOW);
-  }
-}
+Slave FPGA(SPI_CS);
 
 void setup()
 {
@@ -92,14 +72,14 @@ void setup()
   Serial.println("Serial started");
 
   // Connect to Wi-Fi network with SSID and password
-  //WiFi.mode(WIFI_STA);
+  // WiFi.mode(WIFI_STA);
   Serial.print("Connecting to ");
 
   Serial.print(PER_SSID);
   WiFi.begin(PER_SSID, PER_PASSWORD);
 
-  //Serial.println(EAP_SSID);
-  //WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_ANONYMOUS_IDENTITY, EAP_IDENTITY, EAP_PASSWORD, ROOT_CA);
+  // Serial.println(EAP_SSID);
+  // WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_ANONYMOUS_IDENTITY, EAP_IDENTITY, EAP_PASSWORD, ROOT_CA);
 
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -114,12 +94,10 @@ void setup()
   Serial.println(WiFi.localIP());
   server.begin();
 
-  // Instantiate SPI
-  SPI.begin();
-  pinMode(SPI_SS, OUTPUT);
-  digitalWrite(SPI_SS, HIGH);
+  // Begin FPGA SPI Slave
+  FPGA.begin();
 
-  //Initiate LED
+  // Initiate LED
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
   LEDState = LOW;
@@ -132,47 +110,47 @@ void loop()
 
   client = server.available(); // Listen for incoming clients
 
-  if (client) 
+  if (client)
   { // If a new client connects,
     currentTime = millis();
     previousTime = currentTime;
 
-    //Serial.print("New Client: IP "); // print a message out in the serial port
-    //Serial.print(client.localIP());
-    //Serial.print(" PORT ");
-    //Serial.println(client.localPort());
+    Serial.print("New Client: IP "); // print a message out in the serial port
+    Serial.print(client.localIP());
+    Serial.print(" PORT ");
+    Serial.println(client.localPort());
 
-
-    String currentLine = "";       // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) 
+    String currentLine = ""; // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime)
     { // loop while the client's connected
 
       currentTime = millis();
-      if (client.available()) 
+      if (client.available())
       { // if there's bytes to read from the client,
 
         char c = client.read(); // read a byte, then
         Serial.write(c);        // print it out the serial monitor
         request += c;
-        if (c == '\n') 
+        if (c == '\n')
         {
           // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0)
           {
+            Graph graph(request);
+            FPGA.spi_tx_string(graph.to_json().c_str());
+            std::string response = FPGA.spi_rx_string();
+            client.println(response.c_str());
 
-            control(request);
-            client.print("Message received");
             break;
-
-          } 
-          else 
+          }
+          else
           {
             currentLine = "";
           }
         }
-        else if (c != '\r') 
+        else if (c != '\r')
         {
           currentLine += c;
         }
@@ -180,38 +158,13 @@ void loop()
     }
 
     // Clear the header variable
-    request= "";
+    request = "";
     // Close the connection
     client.stop();
     Serial.println("");
     Serial.println("Client disconnected.");
     Serial.println("");
-
   }
 
-  // SPI COMMUNICATION
-
-  char myChar = 'M';
-  // put your main code here, to run repeatedly:
-  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
-
-  // Assert slave select -> begin transfer
-  digitalWrite(SPI_SS, LOW);
-  // SPI.transferBytes(NULL, (uint8_t *)TX_BUF, NUM_CHARS);
-  // SPI.transferBytes((const uint8_t *)TX_BUF, NULL, NUM_CHARS + 1);
-  // SPI.transfer(TX_BUF, NUM_CHARS);
-
-  SPI.transfer(myChar);
-  // SPI.transfer('p');
-  // SPI.transfer('\0');
-  // SPI.transfer('a');
-
-  // Serial.println((const char*)RX_BUF);
-
-  // De-assert slave select -> end of transfer
-  digitalWrite(SPI_SS, HIGH);
-
-  SPI.endTransaction();
-
-  sleep(1);
+  usleep(1);
 }
