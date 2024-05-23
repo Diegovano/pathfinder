d1a2828729a21c3bc7fff9e54f05613afd2180a9
Andres Eduardo Martinez Llorente <andres.m.ll2003@outlook.com>
2024-05-23 20:50:19 +0100

    fix: Refactored 2-way communication using a state machine

--- software/esp/Pathfinder/src/main.cpp
+++ software/esp/Pathfinder/src/main.cpp
@@ -6,7 +6,22 @@
 #include <Graph.h>
 #include <Slave.h>
 
+#define SKIP_RX false
+
 const int SPI_CS = D2; // Slave select is active-low
+const int BUF_LEN = 4;
+
+enum State
+{
+  HOST_LISTEN,
+  HOST_RX,
+  FPGA_TX,
+  FPGA_RX,
+  HOST_TX,
+  RESET
+};
+
+State state;
 
 // WPA2 Personal Authentication
 
@@ -17,6 +32,7 @@ const char *PER_PASSWORD = "U00#10s0";
 WiFiServer server(80);
 WiFiClient client;
 std::string request;
+std::string response;
 
 // LED states
 const int LED = D0;
@@ -29,7 +45,7 @@ unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
-Slave FPGA(SPI_CS);
+Slave FPGA(SPI_CS, BUF_LEN);
 
 void setup()
 {
@@ -70,24 +86,37 @@ void setup()
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
   LEDState = LOW;
+
+  // Initiate state
+  state = HOST_LISTEN;
 }
 
 void loop()
 {
 
-  // WIFI SERVER
+  switch (state)
+  {
+  case HOST_LISTEN:
+  {
+    Serial.println("HOST_LISTEN");
+    client = server.available(); // Listen for incoming clients
 
-  client = server.available(); // Listen for incoming clients
+    if (client)
+    {                                  // If a new client connects,
+      Serial.print("New Client: IP "); // print a message out in the serial port
+      Serial.print(client.localIP());
+      Serial.print(" PORT ");
+      Serial.println(client.localPort());
+      state = HOST_RX;
+    }
+    break;
+  }
 
-  if (client)
-  { // If a new client connects,
+  case HOST_RX:
+  {
+    Serial.println("HOST_RX");
     currentTime = millis();
     previousTime = currentTime;
-
-    Serial.print("New Client: IP "); // print a message out in the serial port
-    Serial.print(client.localIP());
-    Serial.print(" PORT ");
-    Serial.println(client.localPort());
 
     String currentLine = ""; // make a String to hold incoming data from the client
     while (client.connected() && currentTime - previousTime <= timeoutTime)
@@ -107,11 +136,7 @@ void loop()
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0)
           {
-            Graph graph(request);
-            FPGA.spi_tx_string(graph.to_json().c_str());
-            std::string response = FPGA.spi_rx_string();
-            client.println(response.c_str());
-
+            state = FPGA_TX;
             break;
           }
           else
@@ -125,15 +150,57 @@ void loop()
         }
       }
     }
-
-    // Clear the header variable
-    request = "";
-    // Close the connection
-    client.stop();
-    Serial.println("");
-    Serial.println("Client disconnected.");
-    Serial.println("");
+    break;
   }
 
-  usleep(1);
+  case FPGA_TX:
+  {
+    Serial.println("FPGA_TX");
+    FPGA.spi_tx_string(request);
+    #if SKIP_RX
+    state = RESET;
+    #else
+    state = FPGA_RX;
+    #endif
+    break;
+  }
+
+  case FPGA_RX:
+  {
+    Serial.println("FPGA_RX");
+    FPGA.spi_rx_string(response);
+    state = HOST_TX;
+    break;
+  }
+
+  case HOST_TX:
+  {
+    Serial.println("HOST_TX");
+    client.println(response.c_str());
+    Serial.println("SUCCESSFUL EXCHANGE! moving to RESET\n");
+    state = RESET;
+    break;
+  }
+
+  case RESET:
+  {
+    Serial.println("RESET");
+    // clear request and response
+    request = "";
+    response = "";
+
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected, moving to HOST_LISTEN");
+    state = HOST_LISTEN;
+    break;
+  }
+
+  default:
+    Serial.println("default");
+    Serial.println("ERROR! moving to RESET");
+    state = RESET;
+  }
+
+  usleep(1e6);
 }
